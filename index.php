6e5acb2a557b045128ed88a130fbb2c3c885d472
Saurabh Rohankar <74132299+SaurabhRohankar@users.noreply.github.com>
2023-12-13 08:41:06 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,10 @@
-<?php
- print "I am Saurabh\n";
- print "I am UPdattttttttt.......\n"; 
-?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>PHP Test App</title>
+</head>
+<body>
+    <h1>Hello, OpenShift BuildConfig!</h1>
+    <p>This is a simple PHP application.</p>
+</body>
+</html>
